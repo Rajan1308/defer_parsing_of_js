d8c25e22912bb47128e69a5489ccf0a8b3c9c05d
Rajan1308 <rajangpt74@gmial.com>
2020-04-20 00:58:45 +0530

    fixed js

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php 
 
-// defer_parsing_of_js
+// Fixed without Resources are blocking the first paint of your page. Consider delivering critical JS/CSS inline and deferring all non-critical JS/styles. 
+
+function defer_parsing_of_js($url)
+{
+  if (is_admin()) return $url; //don't break WP Admin
+  if (false === strpos($url, '.js')) return $url;
+  if (strpos($url, 'jquery.js')) return $url;
+  return str_replace(' src', ' defer src', $url);
+}
+add_filter('script_loader_tag', 'defer_parsing_of_js', 10);
 
 ?>
